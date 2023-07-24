b91168c8fb152dc93609a2e5663936b7b2102b11
RewoLF3020 <zolotovvladislav305@gmail.com>
2023-07-24 23:49:03 +0300

    class Cart created

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,5 +124,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+CART_SESSION_ID = 'cart'
